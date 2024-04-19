0d23d6a50deedf8cbf5859a6173b5d40b6ee0671
master <main_core@gmail.com>
2024-04-19 21:27:41 +0800

    removing video: adding a still image instead

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,6 @@
-"use client";
-
-import { useState } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
 
 const AboutPage = () => {
-  const [loaded, setLoaded] = useState(false);
-
   return (
     <div>
       <h1 className="text-center text-3xl font-semibold mt-8">
@@ -21,21 +16,16 @@ const AboutPage = () => {
         often seen as a different path from traditional academic routes and can
         open up many exciting career opportunities in different industries.
       </p>
-      {loaded || (
-        <Skeleton className="mx-auto mt-16 w-[90%] h-[225px] max-w-[400px] max-h-[225px] rounded-md" />
-      )}
       <div
-        className={`mx-auto mt-16 w-[90%]  max-w-[400px] max-h-[400px] ${
-          loaded ? "block" : "hidden"
-        }`}
+        className={`mx-auto mt-16 w-[90%] rounded-md  max-w-[400px] max-h-[400px]`}
       >
-        <video
-          controls
-          className="rounded-md"
-          onLoadedData={() => setLoaded(true)}
-        >
-          <source src="/video.mp4" type="video/mp4" />
-        </video>
+        <Image
+          alt="tvl image"
+          src="/tvl.png"
+          width={400}
+          height={225}
+          className="w-full rounded-md"
+        />
       </div>
     </div>
   );
