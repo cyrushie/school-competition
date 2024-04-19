7212ba47c2fbea2019554db5c5ee47467482e124
master <main_core@gmail.com>
2024-04-19 19:01:37 +0800

    adding some feature in the header

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
 import { MdMenu } from "react-icons/md";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -7,23 +9,41 @@ import ToggleDarkButton from "@/components/themes/toggle-dark-button";
 import MobileNav from "@/components/navbar/mobile-nav";
 
 const Navbar = () => {
+  const [header, setHeader] = useState(false);
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+
+    return () => {
+      window.removeEventListener("scroll", scrollYPos);
+    };
+  }, [header]);
+
   return (
-    <nav className="max-container h-[6rem] flex justify-between items-center">
-      <div className="font-semibold text-lg flex space-x-2 items-center">
-        <Image alt="bsanhs logo" src="/logo.png" width={30} height={30} />
-        <h1>BSANHS</h1>
-      </div>
+    <header
+      className={`w-[100vw] bg-background ${
+        header ? "sticky top-0 shadow-md z-30 bg-background/95" : "relative"
+      }`}
+    >
+      <nav className="max-container h-[6rem] flex justify-between items-center">
+        <div className="font-semibold text-lg flex space-x-2 items-center">
+          <Image alt="bsanhs logo" src="/logo.png" width={30} height={30} />
+          <h1>BSANHS</h1>
+        </div>
 
-      <NavlinkList />
+        <NavlinkList />
 
-      <div className="hidden md:block">
-        <ToggleDarkButton />
-      </div>
+        <div className="hidden md:block">
+          <ToggleDarkButton />
+        </div>
 
-      <div className="md:hidden block">
-        <MobileNav />
-      </div>
-    </nav>
+        <div className="md:hidden block">
+          <MobileNav />
+        </div>
+      </nav>
+    </header>
   );
 };
 
